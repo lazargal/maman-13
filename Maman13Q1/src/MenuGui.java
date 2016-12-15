16296ef1a9fa1ded069f0f1cf993d6997b6952ba
lazargal <lazargal@gmail.com>
2016-12-15 22:51:30 +0200

    Add files via upload

--- Maman13Q1/src/MenuGui.java
+++ Maman13Q1/src/MenuGui.java
@@ -1,34 +1,52 @@
+import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class MenuGui
 {
     public static void main(String[] args)
     {
-	try
-	{
-	    MenuFileReader myMenuReader = new MenuFileReader("menu.txt");
-	    ArrayList<MenuItem> loadedMenuItems = myMenuReader.getMenuItemArray();
-	    JFrame mainWindow = new JFrame();
-	    loadedMenuItems.sort(null);
-	    
-	    MenuPanel menuPanel = new MenuPanel(loadedMenuItems);
-	    menuPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
-	    JScrollPane vertScroll = new JScrollPane(menuPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-	    vertScroll.setAlignmentX(Component.LEFT_ALIGNMENT);
-	    mainWindow.add(vertScroll);
-	    mainWindow.setSize(200, 100);
-	    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    mainWindow.setVisible(true);
-	    
-	} 
-	catch (FileNotFoundException | IllegalFileFormat e)
-	{
-	    e.printStackTrace();
-	}
+		try
+		{
+		    MenuFileReader myMenuReader = new MenuFileReader("menu.txt");
+		    ArrayList<MenuItem> loadedMenuItems = myMenuReader.getMenuItemArray();
+		    JFrame mainWindow = new JFrame();
+		    mainWindow.setSize(600, 480);
+		    mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    
+		    loadedMenuItems.sort(null);
+		    
+		    MenuPanel menuPanel = new MenuPanel(loadedMenuItems);
+		    menuPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
+		    JScrollPane vertScroll = new JScrollPane(menuPanel, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		    vertScroll.setAlignmentX(Component.LEFT_ALIGNMENT);
+		    mainWindow.add(vertScroll,BorderLayout.CENTER);
+		    
+		    JButton button = new JButton("Order");
+		    button.addActionListener(menuPanel);
+		    JPanel tempPanel = new JPanel();
+		    tempPanel.setLayout(new BorderLayout());
+		    tempPanel.add(button , BorderLayout.EAST);
+		    mainWindow.add(tempPanel,BorderLayout.SOUTH);
+		    
+		    mainWindow.setVisible(true);
+		    
+		} 
+		catch (FileNotFoundException | IllegalFileFormat e)
+		{
+		    e.printStackTrace();
+		}
     }
+
 }
--- Maman13Q1/src/MenuPanel.java
+++ Maman13Q1/src/MenuPanel.java
@@ -2,15 +2,20 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Formatter;
 
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 
-public class MenuPanel extends JPanel
+public class MenuPanel extends JPanel implements ActionListener
 {
     private ArrayList<MenuItem> m_menuList = null;
     private ArrayList<MenuPanelItem> m_menuItemList = new ArrayList<MenuPanelItem>();
@@ -42,10 +47,10 @@ public class MenuPanel extends JPanel
     
     public void resetMenuState()
     {
-	for(MenuPanelItem tempMenuItem : m_menuItemList)
-	{
-	    tempMenuItem.resetContent();
-	}
+		for(MenuPanelItem tempMenuItem : m_menuItemList)
+		{
+		    tempMenuItem.resetContent();
+		}
     }
     
     public float getTotalOrderPrice()
@@ -58,6 +63,59 @@ public class MenuPanel extends JPanel
     
 	return fTemp;
     }
+
+	@Override
+	public void actionPerformed(ActionEvent event) 
+	{
+		StringBuilder myBuilder = new StringBuilder();
+		for(MenuPanelItem tempMenuItem : m_menuItemList)
+		{
+			if(tempMenuItem.getIsChecked())
+				myBuilder.append(tempMenuItem.toString() + "\r\n");
+		}
+		
+		myBuilder.append("Total order cost = " + getTotalOrderPrice());
+		Object[] options = {"Confirm" , "Update" , "Cancel"}; 
+		int nSelection = JOptionPane.showOptionDialog(this, myBuilder.toString(), "Confirm Order", JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+		switch(nSelection)
+		{
+		case JOptionPane.YES_OPTION:
+			saveOrderToFile(myBuilder.toString());
+			resetMenuState();
+			break;
+		case JOptionPane.NO_OPTION:
+			// do nothing for now
+			break;
+		case JOptionPane.CANCEL_OPTION:
+			resetMenuState();
+			break;		
+		}		
+	}
+	
+	public void saveOrderToFile(String orderInfo)
+	{
+		String strUserInput = JOptionPane.showInputDialog("Enter name and ID");
+		if(strUserInput == null)
+			return;
+		strUserInput.trim();
+		if(strUserInput.isEmpty())
+			return;
+			
+		try 
+		{
+			strUserInput += ".txt";
+			Formatter output = new Formatter(strUserInput);
+			output.format(orderInfo, (Object[]) null);
+			output.close();
+			
+		}
+		catch (FileNotFoundException e) 
+		{
+			
+			e.printStackTrace();
+		}
+		
+	}
     
     
     
